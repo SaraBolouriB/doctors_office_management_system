3fb9098b2da3dcfda4e6d83a4a96eba463b9e89d
SaraBolouriB <boloorisara@gmail.com>
2021-03-18 22:45:06 +0330

    updated models

--- doctors_office_management_system/user_account/models.py
+++ doctors_office_management_system/user_account/models.py
@@ -1,31 +1,31 @@
 from django.db import models
 
 class comment(models.Model):
-    user_id = models.IntegerField(null=False, blank=False)
-    doctor_id = models.IntegerField(null=False, blank=False)
-    comment = models.Textfield(max_length=200, blank=True, null=True)
+    user_id = models.ForeignKey('normal_user', on_delete=models.CASCADE)
+    doctor_id = models.ForeignKey('doctor', on_delete=models.CASCADE)
+    comment = models.Textfield(max_length=200, blank=False, null=False)
     created_on = models.DateTimeField(auto_now_add=False)
     
     class Meta:
         unique_together = ['user_id', 'doctor_id']
 
 class appointment(models.Model):
-    user_id = models.IntegerField(null=False, blank=False)
-    doctor_id = models.IntegerField(null=False, blank=False)
+    user_id = models.ForeignKey('normal_user', on_delete=models.CASCADE)
+    doctor_id = models.ForeignKey('doctor', on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=False)
 
     class Meta:
         unique_together = ['user_id', 'doctor_id']
 
 class working_time(models.Model):
-    doctor_id = models.IntegerField(unique=True, null=False, blank=False)
+    doctor_id = models.ForeignKey('doctor', on_delete=models.CASCADE)
     day = models.DateField()
     start_time = models.Charfield()
     end_time = models.Charfield()
 
 class favorite(models.Model):
-    user_id = models.IntegerField(null=False, blank=False)
-    doctor_id = models.IntegerField(null=False, blank=False)
+    user_id = models.ForeignKey('normal_user', on_delete=models.CASCADE)
+    doctor_id = models.ForeignKey('doctor', on_delete=models.CASCADE)
 
     class Meta:
         unique_together = ['user_id', 'doctor_id']
