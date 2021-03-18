f86d8b591c6680575ee0a3539d329b49091167d1
SaraBolouriB <boloorisara@gmail.com>
2021-03-18 22:00:52 +0330

    updated models

--- doctors_office_management_system/user_account/models.py
+++ doctors_office_management_system/user_account/models.py
@@ -1,22 +1,23 @@
 from django.db import models
 
 class comment(models.Model):
-    user_id = models.IntegerField()
-    doctor_id = models.IntegerField()
+    user_id = models.IntegerField(null=False, blank=False)
+    doctor_id = models.IntegerField(null=False, blank=False)
     comment = models.Textfield(max_length=200, blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=False)
 
 class appointment(models.Model):
-    user_id = models.IntegerField()
-    doctor_id = models.IntegerField()
+    user_id = models.IntegerField(null=False, blank=False)
+    doctor_id = models.IntegerField(null=False, blank=False)
     created_on = models.DateTimeField(auto_now_add=False)
 
 class working_time(models.Model):
-    doctor_id = models.IntegerField()
+    doctor_id = models.IntegerField(null=False, blank=False)
     day = models.DateField()
     start_time = models.Charfield()
     end_time = models.Charfield()
 
 class favorite(models.Model):
-    user_id = models.IntegerField()
-    doctor_id = models.IntegerField()
+    user_id = models.IntegerField(null=False, blank=False)
+    doctor_id = models.IntegerField(null=False, blank=False)
+
