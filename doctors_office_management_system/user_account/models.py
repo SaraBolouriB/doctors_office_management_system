198582fe672890dbbaad43d19d0c681da2c1d654
SaraBolouriB <boloorisara@gmail.com>
2021-03-18 21:23:01 +0330

    added appointment table

--- doctors_office_management_system/user_account/models.py
+++ doctors_office_management_system/user_account/models.py
@@ -5,3 +5,8 @@ class comment(models.Model):
     doctor_id = models.IntegerField()
     comment = models.Textfield(max_length=200, blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
+
+class appointment(models.Model):
+    user_id = models.IntegerField()
+    doctor_id = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
