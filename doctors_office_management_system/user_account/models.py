599a0c41d6a8e864a67595a773e1553ebe304be5
AsiyeB <asiyebahrami.95@gmail.com>
2021-03-19 01:17:51 +0330

    all users table added

--- doctors_office_management_system/user_account/models.py
+++ doctors_office_management_system/user_account/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django import forms
+from phonenumber_field.modelfields import PhoneNumberField
+
+class all_users(models.Model):
+    username = models.CharField(max_length=50,null=False, blank=False, primary_key=True)
+    password = forms.CharField(max_length=32, widget=forms.PasswordInput)
+    role = models.IntegerField(null=False, blank=False)
+
 
 class comment(models.Model):
     user_id = models.IntegerField(null=False, blank=False)
