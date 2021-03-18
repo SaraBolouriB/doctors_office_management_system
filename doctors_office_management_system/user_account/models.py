48316207634680aeabaacc37c51f6a7449a96ae2
SaraBolouriB <boloorisara@gmail.com>
2021-03-18 21:48:04 +0330

    added working_time table

--- doctors_office_management_system/user_account/models.py
+++ doctors_office_management_system/user_account/models.py
@@ -4,9 +4,15 @@ class comment(models.Model):
     user_id = models.IntegerField()
     doctor_id = models.IntegerField()
     comment = models.Textfield(max_length=200, blank=True, null=True)
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=False)
 
 class appointment(models.Model):
     user_id = models.IntegerField()
     doctor_id = models.IntegerField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=False)
+
+class working_time(models.Model):
+    doctor_id = models.IntegerField()
+    day = models.DateField()
+    start_time = models.Charfield()
+    end_time = models.Charfield()
