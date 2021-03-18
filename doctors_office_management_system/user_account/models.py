60521e1486f988393317c3d19b80cae67cc70b0e
SaraBolouriB <boloorisara@gmail.com>
2021-03-18 22:02:26 +0330

    added primary keys

--- doctors_office_management_system/user_account/models.py
+++ doctors_office_management_system/user_account/models.py
@@ -5,14 +5,20 @@ class comment(models.Model):
     doctor_id = models.IntegerField(null=False, blank=False)
     comment = models.Textfield(max_length=200, blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=False)
+    
+    class Meta:
+        unique_together = ['user_id', 'doctor_id']
 
 class appointment(models.Model):
     user_id = models.IntegerField(null=False, blank=False)
     doctor_id = models.IntegerField(null=False, blank=False)
     created_on = models.DateTimeField(auto_now_add=False)
 
+    class Meta:
+        unique_together = ['user_id', 'doctor_id']
+
 class working_time(models.Model):
-    doctor_id = models.IntegerField(null=False, blank=False)
+    doctor_id = models.IntegerField(unique=True, ll=False, blank=False)
     day = models.DateField()
     start_time = models.Charfield()
     end_time = models.Charfield()
@@ -21,3 +27,5 @@ class favorite(models.Model):
     user_id = models.IntegerField(null=False, blank=False)
     doctor_id = models.IntegerField(null=False, blank=False)
 
+    class Meta:
+        unique_together = ['user_id', 'doctor_id']
