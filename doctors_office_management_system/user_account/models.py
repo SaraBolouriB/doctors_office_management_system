fe1acf32eb0e5fac101fe3afb5fb378050881064
SaraBolouriB <boloorisara@gmail.com>
2021-03-18 21:21:11 +0330

    added comment table

--- doctors_office_management_system/user_account/models.py
+++ doctors_office_management_system/user_account/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class comment(models.Model):
+    user_id = models.IntegerField()
+    doctor_id = models.IntegerField()
+    comment = models.Textfield(max_length=200, blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
