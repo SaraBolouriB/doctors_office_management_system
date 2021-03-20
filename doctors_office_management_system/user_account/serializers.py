f3056d3ece239da3172411c8cd87dd2c5b769fdb
AsiyeB <asiyebahrami.95@gmail.com>
2021-03-20 20:04:45 +0330

    comment and appoinment serializer added

--- doctors_office_management_system/user_account/serializers.py
+++ doctors_office_management_system/user_account/serializers.py
@@ -18,3 +18,16 @@ class doctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = doctor
         fields = '__all__'
+class commentSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = comment
+        fields = '__all__'
+
+class appointmentSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = appointment
+        fields = '__all__'
+
+
