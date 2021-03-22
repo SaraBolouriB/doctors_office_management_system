86ad364cae48760024c37ab9bafe25a6843193bf
SaraBolouriB <boloorisara@gmail.com>
2021-03-22 19:16:41 +0430

    urls changed

--- doctors_office_management_system/user_account/urls.py
+++ doctors_office_management_system/user_account/urls.py
@@ -7,7 +7,7 @@ app_name = 'user_account'
 urlpatterns = [
     path('follow/', views.following),
     path('comment/', views.comment),
-    path('search/<str:keyword>/', views.search),
+    path('search/keyword=<str:keyword>/', views.search),
     path('setAppointment/', views.set_appointment),
-    path('showTimes/<str:day>/<str:date>/<int:doctorID>/', views.show_times)
+    path('showTimes/day=<str:day>/date=<str:date>/id=<int:doctorID>/', views.show_times)
 ]
